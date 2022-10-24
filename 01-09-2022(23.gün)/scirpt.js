6c41cb7c3f3799e3739d42e32093520bcca73767
zeynepay2703 <116477431+zeynepay2703@users.noreply.github.com>
2022-10-24 12:54:36 +0300

    Add files via upload

---
+++ 01-09-2022(23.gün)/scirpt.js
@@ -0,0 +1,11 @@
+/*bunların hepsi hazır link*/
+var swiper = new Swiper(".swiper-container",
+{
+    slidesPerView: 5,
+    spaceBetween: 10,
+    pagination:
+    {
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev',
+    }
+});
